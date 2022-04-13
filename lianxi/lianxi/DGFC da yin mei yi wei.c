31bfff2e68a8e0052c0fbdf573f9760cefb1976d
AF帆 <1067713425@qq.com>
2022-04-13 18:13:45 +0800

    6

--- lianxi/lianxi/DGFC da yin mei yi wei.c
+++ lianxi/lianxi/DGFC da yin mei yi wei.c
@@ -1,35 +1,45 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include <stdio.h>
-//
-//void printf_weishu(int x)
-//{
-//	int n = 0;
-//	int b = 1;
-//	while(b)  //shu wei shu
-//	{
-//		n++;
-//		b = x % 10;
-//
-//	}
-//	int i = 0;
-//	int di = 1;
-//	for (i = 0; i < n; i++)
-//	{
-//		di = di * 10;
-//	}
-//	if (a)
-//	{
-//		printf("%d", a);
-//
-//		return printf_weishu();
-//	}
-//
-//}
-//int main()
-//{
-//
-//	int a = 0;
-//	scanf("%d", &a);
-//	printf_weishu(a);
-//	return 0;
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+void printf_weishu(int x)
+{
+	int f = x;
+	int n = 0;
+	int b = 0;
+	int *pb = &b;
+	while(1)  //shu wei shu
+	{
+		n++;
+		*pb = x / 10;
+		x = *pb;
+		if (*pb == 0)
+			break;
+
+	}
+	int i = 1;
+	int di = 1;
+	for (i = 1; i < n; i++)
+	{
+		di = di * 10;
+
+	}
+	int a = f / di;
+	int ret = (f - a * di);
+	if (1)
+	{
+		
+		printf("%d\n", a);
+		if (ret != 0) {
+			return printf_weishu(ret);
+		}
+	}
+
+}
+int main()
+{
+
+	int a = 0;
+	scanf("%d", &a);
+	printf_weishu(a);
+	return 0;
+}
